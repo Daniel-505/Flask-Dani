d424865415678fc845a78ed09ac4901f62f9caab
Daniel-505 <fericos190@gmail.com>
2024-05-15 11:38:41 -0300

    conexion a base de datos

--- peliculas/__init__.py
+++ peliculas/__init__.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+with app.app_context():
+    from . import db
+    db.init_app(app)
 
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/categoria')
+def Actor():
+    consulta = """
+        SELECT first_name, last_name FROM actor
+        ORDER BY last_name, first_name; 
+    """
+    con = db.get_db()
+    res = con.execute(consulta)
+    lista_Actor = res.fetchall()
+    pagina = render_template('Actor.html', actores=lista_Actor)
+
+    return pagina
